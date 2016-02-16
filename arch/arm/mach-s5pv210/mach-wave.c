7ab38d69e479ea405cb385c4a10e8ae8d67d9ac9
Nikolay Volkov <Volk204@mail.ru>
2016-02-16 21:56:55 +0100

    Wave: Add CMA support

--- arch/arm/mach-s5pv210/mach-wave.c
+++ arch/arm/mach-s5pv210/mach-wave.c
@@ -321,6 +321,7 @@ static struct s5p_media_device wave_media_devs[] = {
 		.bank = 0,
 		.memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_MFC0,
 		.paddr = 0,
+		.cmadev = &s3c_device_mfc_l.dev,
 	},
 	[1] = {
 		.id = S5P_MDEV_MFC,
@@ -328,6 +329,7 @@ static struct s5p_media_device wave_media_devs[] = {
 		.bank = 1,
 		.memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_MFC1,
 		.paddr = 0,
+		.cmadev = &s3c_device_mfc_r.dev,
 	},
 	[2] = {
 		.id = S5P_MDEV_FIMC0,
@@ -335,6 +337,7 @@ static struct s5p_media_device wave_media_devs[] = {
 		.bank = 1,
 		.memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMC0,
 		.paddr = 0,
+		.cmadev = &s3c_device_fimc0.dev,
 	},
 	[3] = {
 		.id = S5P_MDEV_FIMC2,
@@ -342,6 +345,7 @@ static struct s5p_media_device wave_media_devs[] = {
 		.bank = 1,
 		.memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMC2,
 		.paddr = 0,
+		.cmadev = &s3c_device_fimc2.dev,
 	},
 	[4] = {
 		.id = S5P_MDEV_JPEG,
@@ -349,6 +353,7 @@ static struct s5p_media_device wave_media_devs[] = {
 		.bank = 0,
 		.memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_JPEG,
 		.paddr = 0,
+		.cmadev = &s3c_device_jpeg.dev,
 	},
 	[5] = {
 		.id = S5P_MDEV_FIMD,
@@ -4217,6 +4222,8 @@ static struct platform_device *wave_devices[] __initdata = {
 #endif
 #ifdef CONFIG_VIDEO_MFC50
 	&s3c_device_mfc,
+	&s3c_device_mfc_l,
+	&s3c_device_mfc_r,
 #endif
 #ifdef	CONFIG_S5P_ADC
 	&s3c_device_adc,
